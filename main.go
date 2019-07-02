cf1fd87b93f533bc273cca527dfa05ce3ae47001
Arthur Chang <arthurc0102@gmail.com>
2019-07-03 00:24:39 +0800

    feat: Log finish time

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "github.com/arthurc0102/dcard-popular-post-notify/app/command"
+import (
+	"fmt"
+	"time"
+
+	"github.com/arthurc0102/dcard-popular-post-notify/app/command"
+)
 
 func main() {
 	command.Run()
+	fmt.Println("Check finish at:", time.Now().Format("2006/01/02 15:04:05 -07:00"))
 }
